700390fe40bfe619fc730547da16a386402f5d94
jaymz campbell <jaymzcd@googlemail.com>
2011-09-14 00:33:58 +0100

    fix disc to result one (53)

--- disc.py
+++ disc.py
@@ -1,12 +1,38 @@
 #!/usr/bin/env python2
-from bisect import bisect
-from collections import defaultdict
+from itertools import combinations
 
 def number_of_disc_intersections(A):
+    boundries = list()
 
-    return c
+    for x, r in enumerate(A):
+        boundries.append((x-r, x+r))
+    
+    r = range(len(A))
+    pairs = combinations(r, 2)
+
+    def overlap(k, j):
+        k, j = boundries[k], boundries[j]
+        if (k[1]>=j[0]):
+            return 1
+        else:
+            return 0
+
+    counts = list()
+
+    for pair in pairs:
+        counts.append(overlap(*pair))
+    
+    return sum(counts)
+
     
 if __name__=='__main__':
-    cnt = number_of_disc_intersections([1, 5, 2, 1, 4, 0])
+    cnt = number_of_disc_intersections(range(2000))
     print "Count is %d" % cnt
     
+""" Result was:
+
+    53/100
+   
+    Time was O**2 :(
+    
+"""
