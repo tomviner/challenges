904d68f982cc6c8e5853aa8dbd982a409a6cf0c7
jaymz campbell <jaymzcd@googlemail.com>
2011-09-14 00:13:55 +0100

    result for prefix - 92.3

--- prefix.py
+++ prefix.py
@@ -13,3 +13,16 @@ def ps(A):
 
 if __name__=='__main__':
     print ps([2, 2, 1, 0, 1])
+
+""" Score: 92.3
+
+Detected time complexity:
+O(N*log(N)) or O(N) or O(N**3)
+
+Failed on 
+
+random_n_10000 
+random test 10 000 elements and values. 1.276 s.    TIMEOUT ERROR 
+running time: >1.28 sec., time limit: 1.10 sec.
+
+"""
