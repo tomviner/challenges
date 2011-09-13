dd8991a4bf997caa773fd579a1dbcd3af0a34c26
jaymz campbell <jaymzcd@googlemail.com>
2011-09-13 22:38:17 +0100

    result :(
    result :(

--- disc.py
+++ disc.py
@@ -2,30 +2,10 @@
 from itertools import combinations
 
 def number_of_disc_intersections(A):
-    boundries = list()
-
-    for x, r in enumerate(A):
-        boundries.append((x-r, x+r))
     
-    r = range(len(A))
-    pairs = combinations(r, 2)
-
-    def overlap(k, j):
-        k, j = boundries[k], boundries[j]
-        if (k[1]>=j[0]):
-            return 1
-        else:
-            return 0
-
-    counts = list()
-
-    for pair in pairs:
-        counts.append(overlap(*pair))
-    
-    return sum(counts)
-
+    return 0
     
 if __name__=='__main__':
-    cnt = number_of_disc_intersections(range(2000))
+    cnt = number_of_disc_intersections([1, 5, 2, 1, 4, 0])
     print "Count is %d" % cnt
     
